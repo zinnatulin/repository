71f56cecaac991212b40b276806d7c2abf703049
Фаиль <zinnatulinfail@gmail.com>
2019-11-12 20:00:57 +0300

    Добавлена функция автоматического скроллинга слайдера.

--- profitkit/test2/script.js
+++ profitkit/test2/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-    $('.lazy_slider').slick();
+    $('.lazy_slider').slick({
+        autoplay:true,//автоматическая прокрутка слайдера. если убрать свойство то слайдер использоуется методом перетаскивания.
+        autoplaySpeed: 1500,
+        dots:true
+    });
 });
 let images = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png'];
+
