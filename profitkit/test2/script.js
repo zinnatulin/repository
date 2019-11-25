6c00b57ba2e542539bd27431eea89212b8a5d38b
Фаиль <zinnatulinfail@gmail.com>
2019-11-25 08:33:08 +0300

    Создание html страницы с loremipsum c подключением js

--- profitkit/test2/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.lazy_slider').slick({
-        autoplay:true,//автоматическая прокрутка слайдера. если убрать свойство то слайдер использоуется методом перетаскивания.
-        autoplaySpeed: 1500,
-        dots:true
-    });
-});
-let images = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png'];
-
