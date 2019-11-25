395c2a607ae4481579264e781814f1ee7bc62b13
Фаиль <zinnatulinfail@gmail.com>
2019-11-25 11:09:51 +0300

    Создан бар прогресса скроллинга страницы

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,20 @@
 window.onload = function(){
     var progressLine = document.getElementById('progress-line');
+    var varbody = document.body;
+    var viewportHeight = window.innerHeight;
+    var documentHeight = Math.max(varbody.scrollHeight, varbody.offsetHeight, varbody.clientHeight);
+    var scrollTopValue = function () {
+        return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body)
+            .scrollTop;
+    }
 
+    window.addEventListener("scroll", function () {
+        var scroll = scrollTopValue();
+        var progress = (scroll / (documentHeight - viewportHeight)) * 100;
+        progressLine.style.width = progress + '%';
+    });
+    
+    window.addEventListener('resize', function () {
+        documentHeight = Math.max(varbody.scrollHeight, varbody.offsetHeight, varbody.clientHeight);
+    })
 }
