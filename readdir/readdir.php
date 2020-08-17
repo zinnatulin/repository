dae3e66ce3f385689f5cced601786c8f24e809dd
Фаиль <zinnatulinfail@gmail.com>
2020-08-17 21:14:46 +0300

    проверка интервалов в отеле

--- readdir/readdir.php
+++ readdir/readdir.php
@@ -13,24 +13,30 @@
 
     echo "чтение директории:<br/>";
     $dir = ".";
-    $arrDate = array();
+    $arrDate = [];
     $arrSize = [];
+    $arrAll = [];
     if($handle = opendir($dir)){
         while($entry = readdir($handle)){
             $time = filemtime($entry);
-            $size = filesize($entry);
+            $size = filesize($entry) . "kB";
             $fileDate = date("d.m.y", $time);
             $fileSize = round($size / 1024, 2);
             $arrDate[$entry] = $fileDate;
-            $arrSize[$entry] = "$fileSize kB";
-            usort($arrDate, "arrCompare");
-            usort($arrSize, "arrCompare");
+            $arrSize[$entry] = $fileSize;
+            $arrAll[] = array($entry, $fileDate, $fileSize);
+            uasort($arrDate, "arrCompare");
+            uasort($arrSize, "arrCompare");
         }
     }
-    foreach ($arrDate as $key => $value){
-        echo "$key $value $arrSize[$key]<br/>";
-    }
-
+    print_r($arrAll);
+    /*foreach ($arrSize as $key => $value){
+        echo "$key $value $arrDate[$key]<br/>";
+    }*/
+    //2 массива: 1 массив: [название => размер], 2 массив: [размер => дата]
+    //сортирую 1 массив по размеру, затем сортирую второй массив по дате.
+    //сформировать массив из 3 элементов, отсортировать массив по размеру, указать в функции
+    //if $a == $b то сортировать по дате.
 ?>
 </body>
 </html>
