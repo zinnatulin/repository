1dfcb26e4e2b31d39f2b74009bfff57dc3491da5
zinnatulin <57392462+zinnatulin@users.noreply.github.com>
2021-07-23 19:16:28 +0300

    Delete auth directory

--- auth/lang/ru/lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-$MESS["AUTH_ADDITIONAL_CONTACT"] = "Возникли сложности? </br>Пишите на <a href='mailto:kilyazov@iac.spb.ru' class='email'>kilyazov@iac.spb.ru</a> или звоните на 576-62-51";
-?>
